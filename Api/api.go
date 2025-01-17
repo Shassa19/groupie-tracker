0f1f4061b666a24f271b2b9c508dab46bab98632
Samuel Bouhnik-Loury <samuel.bouhnikloury@gmail.com>
2025-01-17 10:25:53 +0100

    On créer le serveur web et on lie les 2 fichiers

---
+++ Api/api.go
@@ -0,0 +1,52 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+// On définit la structure de l'Artist
+type Artist struct {
+	ID           int      `json:"id"`
+	Name         string   `json:"name"`
+	Image        string   `json:"image"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+}
+
+var artists []Artist
+
+// Fonction pour récupérer les données depuis l'API
+func FetchArtists(url string) ([]Artist, error) {
+	response, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("Erreur lors de la requête : %v", err)
+	}
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Erreur lors de la lecture de la réponse : %v", err)
+	}
+
+	var fetchedArtists []Artist
+	err = json.Unmarshal(body, &fetchedArtists)
+	if err != nil {
+		return nil, fmt.Errorf("Erreur lors du décodage JSON : %v", err)
+	}
+	return fetchedArtists, nil
+}
+
+// Fonction pour afficher les artistes
+func DisplayArtists(artists []Artist) {
+	for _, artist := range artists {
+		members := strings.Join(artist.Members, ", ")
+		fmt.Printf("Nom: %s,\n ID: %d,\n Image: %s,\n Membres: %v,\n Date de création: %d,\n Premier album: %s\n",
+			artist.Name, artist.ID, artist.Image, members, artist.CreationDate, artist.FirstAlbum)
+		fmt.Println("\n-----------------------------------------------------------------------------------")
+	}
+}
