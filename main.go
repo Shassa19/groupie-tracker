1e8628f3011b2c62e3d2d8024e13f1cc933051c9
Deverchere Geoffrey <geoffreydeverchere@gmail.com>
2025-01-17 09:30:24 +0100

    mise en place de fonction pour le serv

--- main.go
+++ main.go
@@ -18,39 +18,48 @@ type Artist struct {
 	FirstAlbum   string   `json:"firstAlbum"`
 }
 
-func main() {
+var artists []Artist
 
-	url := "https://groupietrackers.herokuapp.com/api/artists"
-
-	// Detecte l'erreur lors de la requête
+// Fonction pour récupérer les données depuis l'API
+func fetchArtists(url string) ([]Artist, error) {
 	response, err := http.Get(url)
 	if err != nil {
-		fmt.Println("Erreur lors de la requête :", err)
-		return
+		return nil, fmt.Errorf("Erreur lors de la requête : %v", err)
 	}
 	defer response.Body.Close()
 
-	// Detecte lorsque ezrreur pdt la lecrure de la réponse
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
-		fmt.Println("Erreur lors de la lecture de la réponse :", err)
-		return
+		return nil, fmt.Errorf("Erreur lors de la lecture de la réponse : %v", err)
 	}
 
-	var artists []Artist
-
-	// Detecte reeur lors du décodage
-	err = json.Unmarshal(body, &artists)
+	var fetchedArtists []Artist
+	err = json.Unmarshal(body, &fetchedArtists)
 	if err != nil {
-		fmt.Println("Erreur lors du décodage JSON :", err)
-		return
+		return nil, fmt.Errorf("Erreur lors du décodage JSON : %v", err)
 	}
+	return fetchedArtists, nil
+}
 
-	// On affiche les donées recuperées
+// Fonction pour afficher les artistes
+func displayArtists(artists []Artist) {
 	for _, artist := range artists {
 		members := strings.Join(artist.Members, ", ")
 		fmt.Printf("Nom: %s,\n ID: %d,\n Image: %s,\n Membres: %v,\n Date de création: %d,\n Premier album: %s\n",
 			artist.Name, artist.ID, artist.Image, members, artist.CreationDate, artist.FirstAlbum)
 		fmt.Println("\n-----------------------------------------------------------------------------------")
 	}
+}
+
+func main() {
+	url := "https://groupietrackers.herokuapp.com/api/artists"
+
+	var err error
+	artists, err = fetchArtists(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	displayArtists(artists)
 }
