d6402fe8607c85c698f2132914864e485b0ca0a8
Deverchere Geoffrey <geoffreydeverchere@gmail.com>
2025-03-04 08:54:40 +0100

    avancement carousel css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,3 @@
-const handle = document.querySelector('.scroll-handle');
-const bar = document.querySelector('.scroll-bar');
-
-let isDragging = false;
-let startX, startLeft;
-
-handle.addEventListener('mousedown', (e) => {
-    isDragging = true;
-    startX = e.clientX;
-    startLeft = handle.offsetLeft;
-    handle.style.cursor = 'grabbing';
-});
-
-document.addEventListener('mousemove', (e) => {
-    if (!isDragging) return;
-    let moveX = e.clientX - startX;
-    let newLeft = startLeft + moveX;
-
-    // Empêcher de sortir des limites
-    if (newLeft < 2) newLeft = 2;
-    if (newLeft > bar.offsetWidth - handle.offsetWidth - 2) { 
-        newLeft = bar.offsetWidth - handle.offsetWidth - 2; /* Ajoute 1px de marge */
-    }
-    
-
-    handle.style.left = newLeft + 'px';
-});
-
-document.addEventListener('mouseup', () => {
-    isDragging = false;
-    handle.style.cursor = 'grab';
-});
-
-
 /** Mode nuit */
 
 const toggleSwitch = document.querySelector('.toggle-mode');
